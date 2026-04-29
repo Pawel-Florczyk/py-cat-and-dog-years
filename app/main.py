d01302688469d34f5ad6f44a482da16d343695fe
Paweł Florczyk <pawelf775@gmail.com>
2026-04-29 19:20:48 +0200

    Solve

--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
 def get_human_age(cat_age: int, dog_age: int) -> list:
     human_age = [0, 0]
-    if 15 <= cat_age and 15 <= dog_age:
+    if 15 <= cat_age:
         human_age[0] = 1
+    if 15 <= dog_age:
         human_age[1] = 1
-    if 24 <= cat_age and 24 <= dog_age:
+    if 24 <= cat_age:
         human_age[0] = 2
+    if 24 <= dog_age:
         human_age[1] = 2
     if cat_age >= 28:
         human_age[0] += (cat_age - 24) // 4
